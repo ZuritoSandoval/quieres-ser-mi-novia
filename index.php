bc0f06d75e7cf7896794bbf0960b47481fe24569
Eduardo Antonio Mendez Sandoval <90435660+ZuritoSandoval@users.noreply.github.com>
2021-11-03 15:37:39 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>¿Quieres ser mi novia?</h1>
+    <div class="botones">
+        <button class="si" onclick="accionParaCuandoEllaDigaQueSi()">SI</button>
+        <button class="no" id="btnNo" onmouseover="mueveElBoton()">NO</button>
+    </div>
+    <script src="js/javascript.js"></script>
+</body>
+</html>
